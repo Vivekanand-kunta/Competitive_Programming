ccaa1377f2abf09281b0121872f2c4f0d99ce043
Vivekanandreddy Kunta <154670452+Vivekanand-kunta@users.noreply.github.com>
2025-08-12 09:36:18 +0530

    Create ans_1.py

---
+++ graphs/ans_1.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+from collections import deque
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+        graph = {node: Node(node.val, [])}
+        q = deque([node])
+        while q:
+            curr = q.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in graph:
+                    graph[neighbor] = Node(neighbor.val, [])
+                    q.append(neighbor)
+                graph[curr].neighbors.append(graph[neighbor])
+        
+        return graph[node]

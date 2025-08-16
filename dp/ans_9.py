ce975f56ea5de4dfe71b5414bdc9c0ed18dab105
Vivekanandreddy Kunta <154670452+Vivekanand-kunta@users.noreply.github.com>
2025-08-16 09:43:24 +0530

    Update ans_9.py

--- dp/ans_9.py
+++ dp/ans_9.py
@@ -1,1 +1,14 @@
-hi
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        minp = nums[0]
+        maxp = nums[0]
+        sol = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                minp, maxp = maxp, minp  
+            maxp = max(nums[i], maxp * nums[i])
+            minp = min(nums[i], minp * nums[i])
+            sol = max(sol, maxp)
+
+        return sol
